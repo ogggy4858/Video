7039f8bbcc6afd1a004de99c6e081f53f750744d
ogggy4858 <35600894+ogggy4858@users.noreply.github.com>
2019-02-21 10:44:11 +0700

    Add files via upload

---
+++ WebYoutube/Models/PeopleDetails.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebYoutube.Models
+{
+    public class PeopleDetails
+    {
+        public string Email { get; set; }
+
+        public string Pass { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Immage { get; set; }
+
+        public DateTime? CreateDatePeople { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Description { get; set; }
+
+        public string Video { get; set; }
+
+        public int? ViewCount { get; set; }
+
+        public DateTime? CreateDatePost { get; set; }
+
+        public bool? StatusPost { get; set; }
+    }
+}

8c98520c729f577e4e0867ce06095ed3526b56c6
ogggy4858 <35600894+ogggy4858@users.noreply.github.com>
2019-02-21 10:50:50 +0700

    Add files via upload

---
+++ WebYoutube/Session/CommonConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebYoutube.Session
+{
+    public class CommonConstants
+    {
+        public static string USER_SESSTION = "USER_SESSTION";
+   
+    }
+}
---
+++ WebYoutube/Session/User.cs
@@ -0,0 +1,9 @@
+﻿namespace WebYoutube.Session
+{
+    public class User
+    {
+        public static int Id { get; set; }
+        public static string Email { get; set; }
+        public static string FullName { get; set; }
+    }
+}
